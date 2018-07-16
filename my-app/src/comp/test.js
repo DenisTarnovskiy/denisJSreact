d5313318b00d0e8730a15474f7468741e9ac1bf0
Denis Tarnovsky <>
2018-07-16 13:23:05 +0300

    no message

--- my-app/src/comp/test.js
+++ my-app/src/comp/test.js
@@ -9,7 +9,7 @@ class Test extends Component {
         'sort_by=popularity.desc&' +
         'include_adult=false&' +
         'include_video=true&' +
-        'primary_release_year=2015&'+
+        'primary_release_year=2018&'+
         'page=1';
     mediaUrl = "http://image.tmdb.org/t/p/w185//";
 
@@ -17,7 +17,8 @@ class Test extends Component {
         super(props);
         this.state = {
             result: [],
-              page: ''
+              page: '',
+            videos: []
         };
     }
 
@@ -27,11 +28,35 @@ class Test extends Component {
             .then(post => {this.setState({result: post.results, page:post.page})});
     }
 
-    getVidio(id){
-        fetch('https://api.themoviedb.org/3/movie/'+ id+'/videos?api_key=95787530723fdf659807136ee61b9df9&language=en-US')
-            .then(res => res.json()).then(videos => videos.results[0].key);
-console.log(this.state.result[0].key)
+    getVideo(id){
+
+        var requestURL = 'https://api.themoviedb.org/3/movie/'+ id +'/videos?api_key=95787530723fdf659807136ee61b9df9&language=en-US'
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+
+        request.responseType = 'json';
+        request.send();
+
+            request.onload = function () {
+                var films = request.response;
+               console.log(films['results'][0]['key']);
+                () => {this.setState({videos: (films['results'][0]['key'])})};
+                return  (films['results'][0]['key']);
+                console.log(films);
+
+
+            };
+        //console.log(this.state.videos);
+return this.state.videos;
     }
+    //     console.log(id);
+    //     fetch('https://api.themoviedb.org/3/movie/'+ id +'/videos?api_key=95787530723fdf659807136ee61b9df9&language=en-US')
+    //     .then(
+    //         res => res.json()
+    //     )
+    //         .then(videos => videos.results.key);
+    //
+    // }
 
 
     render() {
@@ -44,7 +69,7 @@ console.log(this.state.result[0].key)
                         <img src={this.mediaUrl + movie.backdrop_path} alt={movie.title}/>
                         <iframe width="560"
                                 height="315"
-                                src={'https://www.youtube.com/embed/' + this.getVidio(movie.id) }
+                                src={'https://www.youtube.com/embed/' + this.getVideo(movie.id) }
                                 frameBorder="0"
                                 allow="autoplay; encrypted-media" allowFullScreen>
 
